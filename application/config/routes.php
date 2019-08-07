eeede3115f3ec4da1f10f043483cb02662ed5baf
faizal <faizaleko31@gmail.com>
2019-08-07 14:28:39 +0700

    Implement tutorial #3 https://www.petanikode.com/codeigniter-bootstrap/

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['about'] = 'welcome/about';
 $route['contact'] = 'welcome/contact';
+$route['admin'] = 'admin/overview';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
