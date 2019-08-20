ccedebe7a8820d616e1b4a992495518181f95f75
faizal <faizaleko31@gmail.com>
2019-08-20 10:00:59 +0700

    Implement Tutorial #5 & #6 https://www.petanikode.com/codeigniter-database/ https://www.petanikode.com/codeigniter-upload/

---
+++ application/views/admin/product/list.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view("admin/_partials/head.php") ?>
+</head>
+
+<body id="page-top">
+
+	<?php $this->load->view("admin/_partials/navbar.php") ?>
+	<div id="wrapper">
+
+		<?php $this->load->view("admin/_partials/sidebar.php") ?>
+
+		<div id="content-wrapper">
+
+			<div class="container-fluid">
+
+				<?php $this->load->view("admin/_partials/breadcrumb.php") ?>
+
+				<!-- DataTables -->
+				<div class="card mb-3">
+					<div class="card-header">
+						<a href="<?php echo site_url('admin/products/add') ?>"><i class="fas fa-plus"></i> Add New</a>
+					</div>
+					<div class="card-body">
+
+						<div class="table-responsive">
+							<table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+								<thead>
+									<tr>
+										<th>Name</th>
+										<th>Price</th>
+										<th>Photo</th>
+										<th>Description</th>
+										<th>Action</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach ($products as $product): ?>
+									<tr>
+										<td width="150">
+											<?php echo $product->name ?>
+										</td>
+										<td>
+											<?php echo $product->price ?>
+										</td>
+										<td>
+											<img src="<?php echo base_url('upload/product/'.$product->image) ?>"
+												width="64" />
+										</td>
+										<td class="small">
+											<?php echo substr($product->description, 0, 120) ?>...</td>
+										<td width="250">
+											<a href="<?php echo site_url('admin/products/edit/'.$product->product_id) ?>"
+												class="btn btn-small"><i class="fas fa-edit"></i> Edit</a>
+											<a onclick="deleteConfirm('<?php echo site_url('admin/products/delete/'.$product->product_id) ?>')"
+												href="#!" class="btn btn-small text-danger"><i class="fas fa-trash"></i>
+												Hapus</a>
+										</td>
+									</tr>
+									<?php endforeach; ?>
+
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<!-- /.container-fluid -->
+
+			<!-- Sticky Footer -->
+			<?php $this->load->view("admin/_partials/footer.php") ?>
+
+		</div>
+		<!-- /.content-wrapper -->
+
+	</div>
+	<!-- /#wrapper -->
+	<!-- Delete Confirmation-->
+	<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<div class="modal-body">Data yang dihapus tidak akan bisa dikembalikan.</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+				<a id="btn-delete" class="btn btn-danger" href="#">Delete</a>
+			</div>
+			</div>
+		</div>
+	</div>
+
+	<?php $this->load->view("admin/_partials/scrolltop.php") ?>
+	<?php $this->load->view("admin/_partials/modal.php") ?>
+	<?php $this->load->view("admin/_partials/js.php") ?>
+	<script>
+		function deleteConfirm(url) {
+			$('#btn-delete').attr('href', url);
+			$('#deleteModal').modal();
+		}
+	</script>
+</body>
+
+</html>
